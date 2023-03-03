1e298169d3f50ffe806ace562369ab84fb907ce6
hanifi <hanifi2769@gmail.com>
2023-03-03 20:43:11 +0300

    devam edecek

---
+++ src/test/java/day16_notations/C05_Assertions.java
@@ -0,0 +1,45 @@
+package day16_notations;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utilities.TestBase;
+
+public class C05_Assertions extends TestBase {
+
+    @Test
+    public void test01() {
+
+        //amazon anasayfaya gidin
+
+        driver.get("https://www.amazon.com");
+
+        //title in amazon içerdğini test edin
+
+        String expectedTitle="Amazon";
+        String actualTitle=driver.getTitle();
+        Assert.assertTrue(actualTitle.contains(expectedTitle));
+
+        //arama kutusunun erişilebilir olduğunu test edin
+
+        WebElement aramakutusu=driver.findElement(By.id("twotabsearchtextbox"));
+        Assert.assertTrue(aramakutusu.isEnabled());
+
+        //arama kutusuna nutella yazıp aratın
+
+        aramakutusu.sendKeys("Nutella" + Keys.ENTER);
+
+        //arama yapıldığını test edin
+
+        WebElement sonuçYazısıElementi=driver.findElement(By.xpath("//div[@class='a-section a-spacing-small a-spacing-top-small']"));
+        Assert.assertTrue(sonuçYazısıElementi.isDisplayed());
+
+        //arama sonucunun nutella içerdiğini test edin
+
+        Assert.assertTrue(sonuçYazısıElementi.getText().contains("Nutella"));
+
+
+    }
+}
---
+++ src/test/java/day16_notations/C06_SoftAssert.java
@@ -0,0 +1,61 @@
+package day16_notations;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import utilities.TestBase;
+
+public class C06_SoftAssert extends TestBase {
+
+    @Test
+    public void test01() {
+        /*
+        Softassertion başlangıç ve bitiş satırları arasındaki
+        tüm assertion ları yapıp
+        bitiş satırına geldiğinde bize bulduğu hataları rapor eder
+         */
+
+        //başlangıç olarak obje oluşturulur
+
+        SoftAssert softAssert=new SoftAssert();
+
+        //amazon anasayfaya gidin
+
+        driver.get("https://www.amazon.com");
+
+        //title in amazon içerdğini test edin
+
+        String expectedTitle="Amazon";
+        String actualTitle=driver.getTitle();
+        softAssert.assertTrue(actualTitle.contains(expectedTitle),"title amazon içermiyor");
+
+        //arama kutusunun erişilebilir olduğunu test edin
+
+        WebElement aramakutusu=driver.findElement(By.id("twotabsearchtextbox"));
+        softAssert.assertTrue(aramakutusu.isEnabled(),"arama kutusu erişilebilir değil");
+
+        //arama kutusuna nutella yazıp aratın
+
+        aramakutusu.sendKeys("Nutella" + Keys.ENTER);
+
+        //arama yapıldığını test edin
+
+        WebElement sonuçYazısıElementi=driver.findElement(By.xpath("//div[@class='a-section a-spacing-small a-spacing-top-small']"));
+        softAssert.assertTrue(sonuçYazısıElementi.isDisplayed(),"arama yapılamadı");
+
+        //arama sonucunun nutella içerdiğini test edin
+
+        softAssert.assertTrue(sonuçYazısıElementi.getText().contains("nutella"),"arama sonucu nutella içermiyor");
+
+
+        //softassert e bitiş satırını söylemek için assertAll(); deriz
+
+        softAssert.assertAll();
+
+        System.out.println("assertion larda fail ise bu kısım yazılmaz");
+
+    }
+}
