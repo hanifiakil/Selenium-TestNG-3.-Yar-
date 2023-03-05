e7f380be9a4a0af897e490e5c286733f83c946be
hanifi <hanifi2769@gmail.com>
2023-03-05 08:32:10 +0300

    selenium

---
+++ src/test/java/tests/day18_pom/C02_ConfigReaderKullanımı.java
@@ -0,0 +1,33 @@
+package tests.day18_pom;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utilities.Driver;
+
+public class C02_ConfigReaderKullanımı {
+
+    @Test
+    public void test01() {
+        //facebook anasayfaya gidin
+
+
+
+        //kullanıcı mail kutusuna rastgele bir isim yazdırın
+
+
+
+        //kullanıcı şifre kutusuna rastgele bir password yazdırın
+
+
+
+        //login butonuna basın
+
+
+
+        //giriş yapılmadığını test edin
+
+
+
+        Driver.closeDriver();
+    }
+}
