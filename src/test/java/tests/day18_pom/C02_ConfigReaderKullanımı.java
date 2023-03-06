d12f5a6f5751d18d7039daea12b0d61da6a6868d
hanifi <hanifi2769@gmail.com>
2023-03-06 12:55:03 +0300

    selenium

--- src/test/java/tests/day18_pom/C02_ConfigReaderKullanımı.java
+++ src/test/java/tests/day18_pom/C02_ConfigReaderKullanımı.java
@@ -2,31 +2,34 @@ package tests.day18_pom;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.FacebookPage;
+import utilities.ConfigReader;
 import utilities.Driver;
 
 public class C02_ConfigReaderKullanımı {
 
     @Test
     public void test01() {
+        FacebookPage facebookPage=new FacebookPage();
         //facebook anasayfaya gidin
 
+        Driver.getDriver().get(ConfigReader.getProperty("facebookUrl"));
 
+        //kullanıcı mail kutusuna yanlış isim yazdırın
 
-        //kullanıcı mail kutusuna rastgele bir isim yazdırın
+        facebookPage.mailKutusu.sendKeys(ConfigReader.getProperty("fbwrongUsername"));
 
+        //kullanıcı şifre kutusuna yanlış password yazdırın
 
-
-        //kullanıcı şifre kutusuna rastgele bir password yazdırın
-
-
+        facebookPage.sifreKutusu.sendKeys(ConfigReader.getProperty("fbWrongPassword"));
 
         //login butonuna basın
 
-
+        facebookPage.loginTusu.click();
 
         //giriş yapılmadığını test edin
 
-
+Assert.assertTrue(facebookPage.girilemediYaziElementi.isDisplayed());
 
         Driver.closeDriver();
     }
